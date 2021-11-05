f727fe22a458ffbb2b6d09719726a7052670d9dd
tomerc <tomerc@siemplify.co>
2021-11-05 15:00:42 +0200

    add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import {RegisterComponent} from "./register/register.component";
 import {ProductsComponent} from "./products/products.component";
 import {ItemInfoComponent} from "./item-info/item-info.component";
 import {CartComponent} from "./cart/cart.component";
+import {AuthGuard} from "./auth.guard";
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'products', component: ProductsComponent},
-  {path: 'cart', component: CartComponent},
-  {path: 'info', component: ItemInfoComponent}
+  {path: 'products', component: ProductsComponent, canActivate: [AuthGuard]},
+  {path: 'cart', component: CartComponent, canActivate: [AuthGuard]},
+  {path: 'info', component: ItemInfoComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
